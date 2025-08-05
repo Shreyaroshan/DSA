f938bcb3afeb4ba386b9ec417c3f7b3b2f2b560a
Shreyaroshan <148137240+Shreyaroshan@users.noreply.github.com>
2025-08-05 21:21:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,23 +1,22 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int placed = 0; // number of fruits successfully placed
-        int j = 0;      // basket index
+        int c = 0; 
+        int j = 0;      
 
         for (int i = 0; i < fruits.length; i++) {
-            // Find the leftmost available basket that can hold the fruit
+            
             while (j < baskets.length && fruits[i] > baskets[j]) {
                 j++;
             }
 
             // If a valid basket is found
             if (j < baskets.length) {
-                placed++;
-                baskets[j] = 0; // mark the basket as used
-                        // reset basket pointer for next fruit
+                c++;
+                baskets[j] = 0; 
             }
             j = 0;  
         }
 
-        return fruits.length - placed; // return number of unplaced fruits
+        return fruits.length - c
     }
 }
